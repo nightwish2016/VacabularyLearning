7ceb3a0861beaeb6b43e3f7866ec42fa0621a1b4
nightwish2016 <kzhou2017@outlook.com>
2025-03-26 09:15:33 +0800

    enhancement

--- Src/mainLocalyVacabulary.py
+++ Src/mainLocalyVacabulary.py
@@ -3,9 +3,9 @@ import datetime as dt
 if __name__ == "__main__":
     auth="secret_Y6RG2tlin3I8fIK5G4LDioYFqiZYS4oPfCBuc0K75vG"
     databaseid='8bf970e295764a2da8af72ef76a81f86'
-    timeDeltaWithUTC=24
+    timeDeltaWithUTC=8
     wordsCount=15
-    newWordCount=3
+    newWordCount=1
     oldWordCount=6
     knotion=VacabularyLearning()
     knotion.process(auth,databaseid,timeDeltaWithUTC,wordsCount,newWordCount,oldWordCount)
