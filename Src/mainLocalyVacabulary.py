45877fbef31f1712c524eb0a7d966b2d2f8337e3
nightwish2016 <kzhou2017@outlook.com>
2025-03-31 15:28:27 +0800

    enhancement

--- Src/mainLocalyVacabulary.py
+++ Src/mainLocalyVacabulary.py
@@ -1,8 +1,9 @@
 from NotionRecurringTask.VacabularyLearning import VacabularyLearning
 import datetime as dt
+import os
 if __name__ == "__main__":
-    auth="secret_Y6RG2tlin3I8fIK5G4LDioYFqiZYS4oPfCBuc0K75vG"
-    databaseid='8bf970e295764a2da8af72ef76a81f86'
+    auth=os.environ.get('AUTH')
+    databaseid=os.environ.get('NOTION_DB')
     timeDeltaWithUTC=8
     wordsCount=15
     newWordCount=1
