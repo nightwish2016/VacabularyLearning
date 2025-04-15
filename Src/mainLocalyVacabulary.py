a5a598e7566e4a3d915b5243069951f2eb3c79e0
Kevinzhou2016 <kzhou2017@outlook.com>
2025-04-15 10:41:15 +0800

    update daily review word count to 12

--- Src/mainLocalyVacabulary.py
+++ Src/mainLocalyVacabulary.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     auth=os.getenv('AUTH')
     databaseid=os.getenv('NOTION_DB')
     timeDeltaWithUTC=8
-    wordsCount=15
+    wordsCount=12
     newWordCount=2
     oldWordCount=6
     knotion=VacabularyLearning()
